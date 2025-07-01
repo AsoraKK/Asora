119af055499e48dbe521bdb8905c92a3d64d034f
AsoraKK <kyle.kern@asora.co.za>
2025-07-01 21:59:39 +0200

    Add AuthService implementation

---
+++ lib/features/auth/domain/auth_failure.dart
@@ -0,0 +1,13 @@
+class AuthFailure implements Exception {
+  final String message;
+  const AuthFailure._(this.message);
+
+  factory AuthFailure.cancelledByUser() =>
+      const AuthFailure._('Cancelled by user');
+
+  factory AuthFailure.serverError([String message = 'Server error']) =>
+      AuthFailure._(message);
+
+  @override
+  String toString() => message;
+}
